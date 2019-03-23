3641194544e21c87943dd1ddd75a408f955017c1
teabolt <tomasbaltrunas0@gmail.com>
2019-03-23 12:45:49 +0000

    Basic types

---
+++ src/types/types.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // BCIFDVW - 7 basic types (keywords)
+    std::cout << "bool " << sizeof(bool) << endl; // can still indicate "std" namespace explicitly
+    std::cout << "char " << sizeof(char) << endl;
+    std::cout << "int " << sizeof(int) << endl;
+    std::cout << "float " << sizeof(float) << endl;
+    std::cout << "double " << sizeof(double) << endl;
+    std::cout << "void " << sizeof(void) << endl;
+    std::cout << "wide char " << sizeof(wchar_t) << endl;
+
+    /* type modifiers (can combine):
+        signed - negative numbers supported / unsigned - no negative numbers (char, int)
+        short - halves (int) / long - doubles(int, double)
+    */
+    std::cout << "long double " << sizeof(long double) << endl;
+    // std::cout << "signed float" << sizeof(signed float) << endl; // not allowed
+    unsigned int x = -10; // this will overflow to positives
+    signed int y = -10;
+    cout << x << endl;
+    cout << y << endl;
+    return 0;
+}
